71a228e718c0a814d136c0d92665887fe1d5c6f7
Eugene Ekimenko <ekimenko_evgeniyy@mail.ru>
2021-12-05 19:36:46 +0300

    lesson service is covered tests

--- src/test/java/com/ekimenko/spring/rest/SpringRESTtestingsystem/service/course_service/impl/CourseServiceImplTest.java
+++ src/test/java/com/ekimenko/spring/rest/SpringRESTtestingsystem/service/course_service/impl/CourseServiceImplTest.java
@@ -11,11 +11,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class CourseServiceImplTest {
@@ -75,29 +79,135 @@ class CourseServiceImplTest {
 
     @Test
     void getAllCoursesDto() {
+        //given
+        List<Course> courseList = new ArrayList<>();
+        Course anotherCourse = new Course();
+        anotherCourse.setId(2L);
+        anotherCourse.setName("testCourse");
+        anotherCourse.setDescription("testDesc");
+        anotherCourse.setComplete(false);
+        anotherCourse.setLessons(null);
+        anotherCourse.setUsers(null);
+
+        List<CourseDto> expectedCourseDtoList = new ArrayList<>();
+
+        CourseDto courseDto = new CourseDto();
+        courseDto.setId(1L);
+        courseDto.setComplete(false);
+        courseDto.setDescription("testDesc");
+        courseDto.setName("testCourse");
+        courseDto.setUsersId(Collections.emptyList());
+        courseDto.setLessonsId(Collections.emptyList());
+
+        CourseDto anotherCourseDto = new CourseDto();
+        anotherCourseDto.setId(2L);
+        anotherCourseDto.setComplete(false);
+        anotherCourseDto.setDescription("testDesc");
+        anotherCourseDto.setName("testCourse");
+        anotherCourseDto.setUsersId(Collections.emptyList());
+        anotherCourseDto.setLessonsId(Collections.emptyList());
+
+        expectedCourseDtoList.add(courseDto);
+        expectedCourseDtoList.add(anotherCourseDto);
+
+        courseList.add(course);
+        courseList.add(anotherCourse);
+        //when
+        Mockito.when(courseRepository.findAll()).thenReturn(courseList);
+        List<CourseDto> actualCourseDtoList = underTestCourse.getAllCoursesDto();
+
+        //then
+        assertThat(actualCourseDtoList).isEqualTo(expectedCourseDtoList);
     }
 
     @Test
     void getCourseDtoById() {
+        //given
+        CourseDto expectedCourse = new CourseDto();
+        expectedCourse.setId(1L);
+        expectedCourse.setComplete(false);
+        expectedCourse.setDescription("testDesc");
+        expectedCourse.setName("testCourse");
+        expectedCourse.setUsersId(Collections.emptyList());
+        expectedCourse.setLessonsId(Collections.emptyList());
+        //when
+        Mockito.when(courseRepository.findById(1L)).thenReturn(java.util.Optional.ofNullable(course));
+        CourseDto actualCourseDto = underTestCourse.getCourseDtoById(1L);
+        //then
+        assertThat(actualCourseDto).isEqualTo(expectedCourse);
     }
 
     @Test
     void getAllCourses() {
+        //given
+        List<Course> expectedCourseList = new ArrayList<>();
+
+        Course anotherCourse = new Course();
+        anotherCourse.setId(2L);
+        anotherCourse.setName("testCourse");
+        anotherCourse.setDescription("testDesc");
+        anotherCourse.setComplete(false);
+        anotherCourse.setLessons(null);
+        anotherCourse.setUsers(null);
+
+        expectedCourseList.add(course);
+        expectedCourseList.add(anotherCourse);
+
+        //when
+        Mockito.when(courseRepository.findAll()).thenReturn(expectedCourseList);
+        List<Course> actualCourseList = underTestCourse.getAllCourses();
+        //then
+        assertThat(actualCourseList).isEqualTo(expectedCourseList);
     }
 
     @Test
     void addNewCourse() {
+        //given
+        CourseDto expectedCourse = new CourseDto();
+        expectedCourse.setId(1L);
+        expectedCourse.setName("testCourse");
+        expectedCourse.setDescription("testDesc");
+        expectedCourse.setComplete(false);
+        expectedCourse.setLessonsId(Collections.emptyList());
+        expectedCourse.setUsersId(Collections.emptyList());
+        //when
+        Mockito.when(courseRepository.save(course)).thenReturn(course);
+        CourseDto actualCourseDto = underTestCourse.addNewCourse(course);
+        //then
+        assertThat(actualCourseDto).isEqualTo(expectedCourse);
     }
 
     @Test
     void updateCourse() {
+        //given
+        CourseDto expectedCourse = new CourseDto();
+        expectedCourse.setId(1L);
+        expectedCourse.setName("testCourse");
+        expectedCourse.setDescription("testDesc");
+        expectedCourse.setComplete(false);
+        expectedCourse.setLessonsId(Collections.emptyList());
+        expectedCourse.setUsersId(Collections.emptyList());
+        //when
+        Mockito.when(courseRepository.save(course)).thenReturn(course);
+        CourseDto actualCourseDto = underTestCourse.updateCourse(course);
+        //then
+        assertThat(actualCourseDto).isEqualTo(expectedCourse);
     }
 
     @Test
     void getCourseById() {
+        //given
+        Course expectedCourse = course;
+        //when
+        Mockito.when(courseRepository.findById(1L)).thenReturn(java.util.Optional.ofNullable(course));
+        Course actualCourse = underTestCourse.getCourseById(1L);
+        //then
+        assertThat(actualCourse).isEqualTo(expectedCourse);
     }
 
     @Test
     void deleteCourseById() {
+        underTestCourse.deleteCourseById(1L);
+        verify(courseRepository).deleteById(1L);
     }
 }
