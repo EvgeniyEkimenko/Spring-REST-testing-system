0acaa00eade1f30944030395268eb9873a7dd552
Eugene Ekimenko <ekimenko_evgeniyy@mail.ru>
2022-01-06 14:54:22 +0300

    test coverage (AnswerVariantRestController)

--- src/test/java/com/ekimenko/spring/rest/SpringRESTtestingsystem/rest/answer_rest/AnswerVariantRestControllerV1Test.java
+++ src/test/java/com/ekimenko/spring/rest/SpringRESTtestingsystem/rest/answer_rest/AnswerVariantRestControllerV1Test.java
@@ -3,6 +3,7 @@ package com.ekimenko.spring.rest.SpringRESTtestingsystem.rest.answer_rest;
 
 
 import com.ekimenko.spring.rest.SpringRESTtestingsystem.dto.answer_dto.AnswerVariantDto;
+import com.ekimenko.spring.rest.SpringRESTtestingsystem.model.answer.AnswerVariant;
 import com.ekimenko.spring.rest.SpringRESTtestingsystem.service.answer_service.AnswerVariantService;
 import com.ekimenko.spring.rest.SpringRESTtestingsystem.service.question_service.QuestionService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -58,17 +59,99 @@ class AnswerVariantRestControllerV1Test {
 
     @Test
     void getAllAnswerVariants() throws Exception{
+        AnswerVariantDto expectedAnswerVariantDto = new AnswerVariantDto();
+        expectedAnswerVariantDto.setId(1L);
+        expectedAnswerVariantDto.setCorrect(false);
+        expectedAnswerVariantDto.setText("testText");
+        expectedAnswerVariantDto.setAnswerResultId(null);
+        expectedAnswerVariantDto.setQuestionId(null);
+
+        List<AnswerVariantDto> expectedAnswerVariantDtoList = new ArrayList<>();
+        expectedAnswerVariantDtoList.add(expectedAnswerVariantDto);
+
+        when(answerVariantService
+                .getAllAnswerVariantsDto())
+                .thenReturn(expectedAnswerVariantDtoList);
+
+        mvc.perform(MockMvcRequestBuilders
+                        .get("/api/v1/answer-variant/all")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
     @Test
     void addNewAnswerVariant() throws Exception{
+        AnswerVariantDto expectedAnswerVariantDto = new AnswerVariantDto();
+        expectedAnswerVariantDto.setId(1L);
+        expectedAnswerVariantDto.setCorrect(false);
+        expectedAnswerVariantDto.setText("testText");
+        expectedAnswerVariantDto.setAnswerResultId(null);
+        expectedAnswerVariantDto.setQuestionId(null);
+
+        AnswerVariant answerVariant = new AnswerVariant();
+        answerVariant.setCorrect(false);
+        answerVariant.setText("testText");
+        answerVariant.setAnswerResult(null);
+        answerVariant.setQuestion(null);
+
+        AnswerVariantDto inputAnswerVariantDto = new AnswerVariantDto();
+        inputAnswerVariantDto.setCorrect(false);
+        inputAnswerVariantDto.setText("testText");
+        inputAnswerVariantDto.setAnswerResultId(null);
+        inputAnswerVariantDto.setQuestionId(null);
+
+        when(answerVariantService
+                .addNewAnswerVariant(answerVariant))
+                .thenReturn(expectedAnswerVariantDto);
+
+        mvc.perform(MockMvcRequestBuilders
+                        .post("/api/v1/answer-variant")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(inputAnswerVariantDto)))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
     @Test
     void updateAnswerVariant() throws Exception{
+        AnswerVariantDto expectedAnswerVariantDto = new AnswerVariantDto();
+        expectedAnswerVariantDto.setId(1L);
+        expectedAnswerVariantDto.setCorrect(false);
+        expectedAnswerVariantDto.setText("testText");
+        expectedAnswerVariantDto.setAnswerResultId(null);
+        expectedAnswerVariantDto.setQuestionId(null);
+
+        AnswerVariant answerVariant = new AnswerVariant();
+        answerVariant.setId(1L);
+        answerVariant.setCorrect(false);
+        answerVariant.setText("testText");
+        answerVariant.setAnswerResult(null);
+        answerVariant.setQuestion(null);
+
+        AnswerVariantDto inputAnswerVariantDto = new AnswerVariantDto();
+        inputAnswerVariantDto.setId(1L);
+        inputAnswerVariantDto.setCorrect(false);
+        inputAnswerVariantDto.setText("testText");
+        inputAnswerVariantDto.setAnswerResultId(null);
+        inputAnswerVariantDto.setQuestionId(null);
+
+        when(answerVariantService
+                .updateAnswerVariant(answerVariant))
+                .thenReturn(expectedAnswerVariantDto);
+
+        mvc.perform(MockMvcRequestBuilders
+                        .post("/api/v1/answer-variant")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(inputAnswerVariantDto)))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
     @Test
     void deleteAnswerVariantByID() throws Exception{
+        mvc.perform(MockMvcRequestBuilders.delete("/api/v1/answer-variant/{id}", 1L))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 }
